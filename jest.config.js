2fee5840fd02379a20cd0512309a85201cc8dad8
Miguel Gandia <miguelgandiajorda@gmail.com>
2025-01-12 21:02:08 +0100

    Test13

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    testMatch: [
+      "**/__tests__/**/*.{test,spec}.{js,jsx,ts,tsx}",
+      "**/?(*.)+(test|spec).[tj]s?(x)"
+    ],
+    testPathIgnorePatterns: ["/node_modules/", "/build/"]
+  };
+  
